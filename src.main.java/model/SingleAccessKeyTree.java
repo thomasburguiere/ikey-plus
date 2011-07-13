764a48c37cc1b0c7764a717958e698eb11bc5077
Thomas Burguiere <thomas.burguiere@upmc.fr>
2011-07-13 13:14:08 +0000

    représentation en HTML de la clé de décision

--- src.main.java/model/SingleAccessKeyTree.java
+++ src.main.java/model/SingleAccessKeyTree.java
@@ -79,6 +79,45 @@ public class SingleAccessKeyTree {
 			recursivetoString(childNode, output, tabulations);
 		}
 	}
+	
+	public void recursiveToHTMLString(SingleAccessKeyNode node, StringBuffer output, String tabulations) {
+		String characterName = null;
+		String state = null;
+		if(node != null && node.getCharacter() != null && node.getCharacterState() != null){
+			characterName = node.getCharacter().getName();
+
+			if(node.getCharacterState() instanceof QuantitativeMeasure)
+				state = ((QuantitativeMeasure) node.getCharacterState()).toStringInterval();
+			else
+				state = ((State) node.getCharacterState()).getName();
+
+			output.append(tabulations + "\t<li>" + characterName);
+
+			if(node.hasChild()){
+				output.append(" | " + state + " - taxa=" + node.getRemainingTaxa().size());
+			}else{
+				output.append(" | " + state + " -> taxa=");
+				for(Taxon taxon : node.getRemainingTaxa()){
+					output.append(taxon.getName() + ",");
+				}
+			}
+			if(node.hasChild())
+				output.append("<ul>");
+			output.append(System.getProperty("line.separator"));
+			tabulations = tabulations + "\t";
+		}
+		for(SingleAccessKeyNode childNode : node.getChildren()){
+			recursiveToHTMLString(childNode, output, tabulations);
+		}
+		if(node != null && node.getCharacter() != null && node.getCharacterState() != null){
+			
+			if(node.hasChild())
+				output.append(tabulations+ "</li></ul>\n");
+			else
+				output.append(tabulations+ "</li>\n");
+			
+		}
+	}
 
 	/* (non-Javadoc)
 	 * 
@@ -88,6 +127,51 @@ public class SingleAccessKeyTree {
 		recursivetoString(root, output, System.getProperty("line.separator"));
 		return output.toString();
 	}
+	
+	/**
+	 * get a HTML file containing the key
+	 * 
+	 * @return File, the html file
+	 * @throws ParserConfigurationException
+	 */
+	public String toHtmlString() {
+		String lineSep = System.getProperty("line.separator");
+		StringBuffer slk = new StringBuffer();
+		slk.append("<html>" + lineSep);
+		slk.append("<head>" + lineSep);
+		slk.append("<link rel='STYLESHEET' type='text/css' href='/Users/thomas/Desktop/dhtmlxTree/dhtmlxTree/codebase/dhtmlxtree.css'/>"
+				+ lineSep
+				+ "<script type='text/javascript'  src='/Users/thomas/Desktop/dhtmlxTree/dhtmlxTree/codebase/dhtmlxcommon.js'></script>"
+				+ lineSep
+				+ "<script type='text/javascript'  src='/Users/thomas/Desktop/dhtmlxTree/dhtmlxTree/codebase/dhtmlxtree.js'></script>"
+				+ lineSep
+				+ "<script type='text/javascript'  src='/Users/thomas/Desktop/dhtmlxTree/dhtmlxTree/codebase/ext/dhtmlxtree_start.js'></script>"
+				+ lineSep);
+
+		slk.append("</head>" + lineSep);
+
+		slk.append("<body>" + lineSep);
+		slk.append("<div class='dhtmlxTree' id='treeboxbox_tree' setImagePath='/Users/thomas/Desktop/dhtmlxTree/dhtmlxTree/codebase/imgs/' >"
+				+ lineSep);
+
+		slk.append("<ul>" + lineSep);
+
+		StringBuffer output = new StringBuffer();
+
+		recursiveToHTMLString(root, output, "");
+
+		slk.append(output.toString());
+
+		slk.append("</ul>" + lineSep);
+		slk.append("</div>" + lineSep);
+
+		slk.append("</body>");
+		slk.append("</html>");
+
+		return slk.toString();
+
+	}
+	
 
 	/**
 	 * get a HTML file containing the key
