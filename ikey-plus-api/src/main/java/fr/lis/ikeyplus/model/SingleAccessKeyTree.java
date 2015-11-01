95a7c14129b7a95d04febd222db2bf323f7693d2
Thomas Burguiere <thomas.burguiere@gmail.com>
2015-11-01 19:37:58 +0100

    removed useless javadoc

--- ikey-plus-api/src/main/java/fr/lis/ikeyplus/model/SingleAccessKeyTree.java
+++ ikey-plus-api/src/main/java/fr/lis/ikeyplus/model/SingleAccessKeyTree.java
@@ -15,96 +15,40 @@ public class SingleAccessKeyTree {
     // the config object (containing options)
     private IkeyConfig config = null;
 
-    /**
-     * constructor
-     */
     public SingleAccessKeyTree(IkeyConfig config) {
         this(null, config);
     }
 
-    /**
-     * constructor with root node param
-     *
-     * @param SingleAccessKeyNode , the root node
-     */
     public SingleAccessKeyTree(SingleAccessKeyNode root, IkeyConfig utils) {
         super();
         this.root = root;
         this.config = utils;
     }
 
-    /**
-     * get the root node
-     *
-     * @return SingleAccessKeyNode, the root node
-     */
     public SingleAccessKeyNode getRoot() {
         return root;
     }
 
-    /**
-     * set the root node
-     *
-     * @param SingleAccessKeyNode , the root node
-     */
     public void setRoot(SingleAccessKeyNode root) {
         this.root = root;
     }
 
-    /**
-     * get the label of the key
-     *
-     * @return String, the label
-     */
     public String getLabel() {
         return dataSet.getLabel();
     }
 
-    /**
-     * get the dataset
-     *
-     * @return DataSet , the dataSet
-     */
     public DataSet getDataSet() {
         return dataSet;
     }
 
-    /**
-     * set the dataset
-     *
-     * @param String , the label
-     */
     public void setDataSet(DataSet dataSet) {
         this.dataSet = dataSet;
     }
 
-    /**
-     * get the config object
-     *
-     * @return IkeyConfig, the config object
-     */
     public IkeyConfig getConfig() {
         return config;
     }
 
-    /**
-     * set the config object
-     *
-     * @param Utils , the config object
-     */
-    public void setConfig(IkeyConfig config) {
-        this.config = config;
-    }
-
-    /**
-     * recursively method to be abbe to display String representation of this SingleAccessKeyTree
-     *
-     * @param node
-     * @param output
-     * @param tabulations
-     * @param firstNumbering
-     * @param secondNumbering
-     */
     private void recursiveToString(SingleAccessKeyNode node, StringBuffer output, String tabulations,
                                    int firstNumbering, int secondNumbering) {
 
@@ -161,9 +105,6 @@ public class SingleAccessKeyTree {
     /**
      * Analyses the node description and returns it if it is not an empty string, and if the verbose level
      * requires it to be displayed. Returns an empty String otherwise.
-     *
-     * @param node
-     * @return
      */
     public String nodeDescriptionAnalysis(SingleAccessKeyNode node) {
         if (node.getNodeDescription() != null && node.getNodeDescription().trim().length() > 0
@@ -173,9 +114,6 @@ public class SingleAccessKeyTree {
         return "";
     }
 
-    /**
-     * this method calls the private {@link recursiveTaxonPathStatistics} method
-     */
     public void gatherTaxonPathStatistics() {
         recursiveTaxonPathStatistics(root, 1);
     }
@@ -183,9 +121,6 @@ public class SingleAccessKeyTree {
     /**
      * This traverses the SingleAccessKeyTree depth-first, and updates the path length statistics for each
      * taxon present in a terminal node
-     *
-     * @param node
-     * @param treeDepth
      */
     private void recursiveTaxonPathStatistics(SingleAccessKeyNode node, int treeDepth) {
         if (node != null && node.getCharacter() != null && node.getCharacterState() != null) {
