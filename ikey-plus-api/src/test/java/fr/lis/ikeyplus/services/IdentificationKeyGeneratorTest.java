95d7415b4c36a989431b47c766e23be6f91a64d8
Thomas Burguiere <thomas.burguiere@gmail.com>
2015-11-03 00:19:59 +0100

    coverage improvement, testing more key generation options (jaccard, sokalAndMichener scores)

--- ikey-plus-api/src/test/java/fr/lis/ikeyplus/services/IdentificationKeyGeneratorTest.java
+++ ikey-plus-api/src/test/java/fr/lis/ikeyplus/services/IdentificationKeyGeneratorTest.java
@@ -94,6 +94,86 @@ public class IdentificationKeyGeneratorTest {
         }
     }
 
+
+
+    @Test
+    public void should_generate_genetta_identification_key_with_jaccard_score_option() throws Exception {
+        String stringUrl = "src/test/resources/inputFiles/genetta.sdd.xml";
+
+        IkeyConfig config = IkeyConfig.builder()
+                .enablePruning()
+                .verbosity(Sets.newHashSet(HEADER, OTHER, WARNING, STATISTICS))
+                .scoreMethod(IkeyConfig.ScoreMethod.JACCARD)
+                .build();
+
+        SDDSaxParser sddSaxParser;
+        try {
+            sddSaxParser = new SDDSaxParser(stringUrl, config);
+
+            IdentificationKeyGenerator identificationKeyGenerator = null;
+
+            try {
+                identificationKeyGenerator = new IdentificationKeyGenerator(sddSaxParser.getDataset(), config);
+                identificationKeyGenerator.createIdentificationKey();
+                SingleAccessKeyTree tree2dump = identificationKeyGenerator.getSingleAccessKeyTree();
+
+                byte[] encoded = Files.readAllBytes(Paths.get("src/test/resources/fixtures/genetta_jaccard.txt"));
+                String genettaFixture = new String(encoded, "UTF-8");
+                assertThat(genettaFixture).isEqualTo(tree2dump.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw (e);
+            }
+
+        } catch (SAXException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @Test
+    public void should_generate_genetta_identification_key_with_sokalAndMichener_score_option() throws Exception {
+        String stringUrl = "src/test/resources/inputFiles/genetta.sdd.xml";
+
+        IkeyConfig config = IkeyConfig.builder()
+                .enablePruning()
+                .verbosity(Sets.newHashSet(HEADER, OTHER, WARNING, STATISTICS))
+                .scoreMethod(IkeyConfig.ScoreMethod.SOKAL_AND_MICHENER)
+                .build();
+
+        SDDSaxParser sddSaxParser;
+        try {
+            sddSaxParser = new SDDSaxParser(stringUrl, config);
+
+            IdentificationKeyGenerator identificationKeyGenerator = null;
+
+            try {
+                identificationKeyGenerator = new IdentificationKeyGenerator(sddSaxParser.getDataset(), config);
+                identificationKeyGenerator.createIdentificationKey();
+                SingleAccessKeyTree tree2dump = identificationKeyGenerator.getSingleAccessKeyTree();
+
+                byte[] encoded = Files.readAllBytes(Paths.get("src/test/resources/fixtures/genetta_sokal_michener.txt"));
+                String genettaFixture = new String(encoded, "UTF-8");
+                assertThat(genettaFixture).isEqualTo(tree2dump.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw (e);
+            }
+
+        } catch (SAXException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
     @Test
     public void should_generate_cichorieae_identification_key_with_default_options() throws Exception {
         String stringUrl = "src/test/resources/inputFiles/cichorieae.sdd.xml";
