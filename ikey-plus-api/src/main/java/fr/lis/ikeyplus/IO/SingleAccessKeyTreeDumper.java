1b26a9b21ccfa5a2f4d08ecb0b2295555e698318
Thomas Burguiere <thomas.burguiere@gmail.com>
2015-11-16 14:52:06 +0100

    [buildfix] missing import

--- ikey-plus-api/src/main/java/fr/lis/ikeyplus/IO/SingleAccessKeyTreeDumper.java
+++ ikey-plus-api/src/main/java/fr/lis/ikeyplus/IO/SingleAccessKeyTreeDumper.java
@@ -18,6 +18,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
