2ccd3bb9fb24e75bf8358bb9f90d247a98be3db1
Thomas Burguiere <thomas.burguiere@gmail.com>
2015-11-23 21:12:09 +0100

    bulk formatting of js files in web module

--- ikey-plus-web/src/main/webapp/app/scripts/controllers/about.js
+++ ikey-plus-web/src/main/webapp/app/scripts/controllers/about.js
@@ -8,10 +8,10 @@
  * Controller of the webappApp
  */
 angular.module('webappApp')
-  .controller('AboutCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('AboutCtrl', function () {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
--- ikey-plus-web/src/main/webapp/test/spec/controllers/about.js
+++ ikey-plus-web/src/main/webapp/test/spec/controllers/about.js
@@ -2,22 +2,22 @@
 
 describe('Controller: AboutCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('webappApp'));
+    // load the controller's module
+    beforeEach(module('webappApp'));
 
-  var AboutCtrl,
-    scope;
+    var AboutCtrl,
+        scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    AboutCtrl = $controller('AboutCtrl', {
-      $scope: scope
-      // place here mocked dependencies
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        AboutCtrl = $controller('AboutCtrl', {
+            $scope: scope
+            // place here mocked dependencies
+        });
+    }));
+
+    it('should attach a list of awesomeThings to the scope', function () {
+        expect(AboutCtrl.awesomeThings.length).toBe(3);
     });
-  }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(AboutCtrl.awesomeThings.length).toBe(3);
-  });
 });
