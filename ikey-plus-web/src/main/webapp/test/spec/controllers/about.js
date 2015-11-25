229d0feb35e753c640cffad00a3c7113db453a1f
Thomas Burguiere <thomas.burguiere@gmail.com>
2015-11-25 07:09:28 +0100

    using chrome runner for tests, fixing tests

--- ikey-plus-web/src/main/webapp/test/spec/controllers/about.js
+++ ikey-plus-web/src/main/webapp/test/spec/controllers/about.js
@@ -11,7 +11,7 @@ describe('Controller: AboutController', function () {
     // Initialize the controller and a mock scope
     beforeEach(inject(function ($controller, $rootScope) {
         scope = $rootScope.$new();
-        AboutCtrl = $controller('AboutCtrl', {
+        AboutCtrl = $controller('AboutController', {
             $scope: scope
             // place here mocked dependencies
         });
--- ikey-plus-web/src/main/webapp/test/spec/controllers/main.js
+++ ikey-plus-web/src/main/webapp/test/spec/controllers/main.js
@@ -11,7 +11,7 @@ describe('Controller: MainController', function () {
     // Initialize the controller and a mock scope
     beforeEach(inject(function ($controller, $rootScope) {
         scope = $rootScope.$new();
-        MainCtrl = $controller('MainCtrl', {
+        MainCtrl = $controller('MainController', {
             $scope: scope
             // place here mocked dependencies
         });
