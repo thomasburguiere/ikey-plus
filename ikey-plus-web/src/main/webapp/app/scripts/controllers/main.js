291b483ac15860f0e7bc366db17a418b210b2584
Thomas Burguiere <thomas.burguiere@gmail.com>
2015-11-25 03:36:50 +0100

    showing service status and version

--- ikey-plus-web/src/main/webapp/app/scripts/controllers/main.js
+++ ikey-plus-web/src/main/webapp/app/scripts/controllers/main.js
@@ -9,10 +9,11 @@
  */
 angular.module('ikey')
     .controller('MainController', function ($http) {
-        var isServiceOnline = $http({url: 'http://localhost:8080/ikey-rest/', method: 'GET'}).then(function (result) {
-            vm.serviceInfo = result.data;
+        $http({url: 'http://localhost:8080/ikey-rest/', method: 'GET'}).then(function (result) {
+            vm.serviceInfo = {version: result.data.version, status: (result.status === 200) ? 'online':'offline'};
         });
         var vm = this;
+        //vm.
         vm.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
