4a8c74d331075e8921f9bf7b88dee9f9dbd16729
Thomas Burguiere <thomas.burguiere@gmail.com>
2015-11-27 06:18:11 +0100

    using factory instead of service for rest resource, using constant to get rest api root

--- ikey-plus-web/src/main/webapp/app/scripts/resources/rest.service.resource.js
+++ ikey-plus-web/src/main/webapp/app/scripts/resources/rest.service.resource.js
@@ -1,23 +1,26 @@
 'use strict';
 angular.module('ikey')
-    .service('SystemInfoResource', ['$http', '$q', function ($http, $q) {
-        this.getServiceInfo = function () {
+    .factory('SystemInfoResource', ['$http', '$q', 'settings', function ($http, $q, settings) {
+        return {
+            getServiceInfo: function () {
 
-            var deferred = $q.defer();
+                var deferred = $q.defer();
 
-            $http({url: 'http://localhost:8080/ikey-rest/', method: 'GET'}).then(
-                function (success) {
-                    deferred.resolve({
-                        version: success.data.version,
-                        status: (success.status === 200) ? 'online' : 'offline'
+                $http({url: settings.REST_API, method: 'GET'}).then(
+                    function (success) {
+                        deferred.resolve({
+                            version: success.data.version,
+                            status: (success.status === 200) ? 'online' : 'offline'
+                        });
+                    },
+                    function () { // error
+                        deferred.resolve({
+                            version: 'unknown',
+                            status: 'offline'
+                        });
                     });
-                },
-                function () { // error
-                    deferred.resolve({
-                        version: 'unknown',
-                        status: 'offline'
-                    });
-                });
-            return deferred;
+                return deferred;
+            }
         };
+
     }]);
