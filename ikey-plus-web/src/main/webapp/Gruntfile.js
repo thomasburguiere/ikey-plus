ed0573b96dbd27f1a0b401ba8f0c9b2d07a30b53
Thomas Burguiere <thomas.burguiere@gmail.com>
2015-11-27 14:06:54 +0100

    changing livereload port

--- ikey-plus-web/src/main/webapp/Gruntfile.js
+++ ikey-plus-web/src/main/webapp/Gruntfile.js
@@ -73,7 +73,7 @@ module.exports = function (grunt) {
                 port: 10000,
                 // Change this to '0.0.0.0' to access the server from outside.
                 hostname: 'localhost',
-                livereload: 35729
+                livereload: 35730
             },
             livereload: {
                 options: {
