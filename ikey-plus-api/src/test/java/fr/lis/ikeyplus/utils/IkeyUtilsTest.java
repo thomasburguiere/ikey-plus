ac5a22565f2e8496adcd23110f5efede260944e7
Thomas Burguiere <thomas.burguiere@gmail.com>
2026-08-30 13:41:19 +0200

    feat: enable github action mvn workflow

--- ikey-plus-api/src/test/java/fr/lis/ikeyplus/utils/IkeyUtilsTest.java
+++ ikey-plus-api/src/test/java/fr/lis/ikeyplus/utils/IkeyUtilsTest.java
@@ -1,7 +1,6 @@
 package fr.lis.ikeyplus.utils;
 
 
-import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 
 import java.util.List;
@@ -10,21 +9,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class IkeyUtilsTest {
 
-    private static final List<String> LIST_A_B_C = ImmutableList.of("A", "B", "C");
-    private static final List<String> LIST_C_D_E = ImmutableList.of("C", "D", "E");
+    private static final List<String> LIST_A_B_C = List.of("A", "B", "C");
+    private static final List<String> LIST_C_D_E = List.of("C", "D", "E");
 
     @Test
     public void should_return_the_intersection() {
-        assertThat(IkeyUtils.intersection(LIST_A_B_C, LIST_C_D_E)).containsOnly("C");
+        assertThat(IkeyUtils.intersection(LIST_A_B_C, LIST_C_D_E)).isEqualTo(List.of("C"));
     }
 
     @Test
     public void should_return_the_exclusion() {
-        assertThat(IkeyUtils.exclusion(LIST_A_B_C, LIST_C_D_E)).containsOnly("A", "B");
+        assertThat(IkeyUtils.exclusion(LIST_A_B_C, LIST_C_D_E)).isEqualTo(List.of("A", "B"));
     }
 
     @Test
     public void should_return_the_union() {
-        assertThat(IkeyUtils.union(LIST_A_B_C, LIST_C_D_E)).containsOnly("A", "B", "C", "D", "E");
+        assertThat(IkeyUtils.union(LIST_A_B_C, LIST_C_D_E)).isEqualTo(List.of("A", "B", "C", "D", "E"));
     }
 }
