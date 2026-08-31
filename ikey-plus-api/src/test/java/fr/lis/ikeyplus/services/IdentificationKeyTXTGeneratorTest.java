c8503301f70c54a1c31c9a0ae8581a6211be1115
Thomas Burguiere <thomas.burguiere@gmail.com>
2026-08-31 20:28:04 +0200

    refactor: parameterized txt dump test

--- ikey-plus-api/src/test/java/fr/lis/ikeyplus/services/IdentificationKeyTXTGeneratorTest.java
+++ ikey-plus-api/src/test/java/fr/lis/ikeyplus/services/IdentificationKeyTXTGeneratorTest.java
@@ -6,6 +6,8 @@ import fr.lis.ikeyplus.IO.SingleAccessKeyTreeDumper;
 import fr.lis.ikeyplus.model.SingleAccessKeyTree;
 import fr.lis.ikeyplus.utils.IkeyConfigBuilder;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.File;
 import java.util.logging.Logger;
@@ -29,10 +31,21 @@ import static fr.lis.ikeyplus.utils.IkeyConfig.getBundleConfOverridableElement;
  */
 public class IdentificationKeyTXTGeneratorTest {
 
-    public Logger logger = Logger.getAnonymousLogger();
-
-    @Test
-    public void testIdentificationKeyGenerator() throws Exception {
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "src/test/resources/inputFiles/cichorieae.sdd.xml",
+            "src/test/resources/inputFiles/Cichorieae-unknownData-fullSDD.xml",
+            "src/test/resources/inputFiles/genetta.sdd.xml",
+             "src/test/resources/inputFiles/milichia_revision-sdd.xml",
+             "src/test/resources/inputFiles/testSDD.xml",
+             "src/test/resources/inputFiles/feuillesSDD.xml",
+             "src/test/resources/inputFiles/feuillesImagesURL.xml",
+             "src/test/resources/inputFiles/smallSDD.xml",
+             "src/test/resources/inputFiles/wrongSDD.xml",
+             "src/test/resources/inputFiles/pruningSDD.xml",
+             "src/test/resources/inputFiles/archaeoSDD.xml"
+    })
+    public void testIdentificationKeyGenerator(final String testFileUrl) throws Exception {
 
         // creation of IkeyConfig object (containing options)
         final IkeyConfigBuilder configBuilder = builder();
@@ -44,42 +57,17 @@ public class IdentificationKeyTXTGeneratorTest {
         final var config = configBuilder.build();
 
 
-        // String containing the name of the result file
         String resultFileName = "";
-        // define logger
-        logger.info("testIdentificationKeyGenerator");
-        // define time before parsing SDD file
         long beforeTime = System.currentTimeMillis();
 
-        // define header string
         final StringBuilder header = new StringBuilder();
 
-        final String stringUrl = "src/test/resources/inputFiles/cichorieae.sdd.xml";
-        // String stringUrl =
-        // "http://www.infosyslab.fr/vibrant/project/test/Cichorieae-unknownData-fullSDD.xml";
-        // String stringUrl = "http://www.infosyslab.fr/vibrant/project/test/phlebotomes-SDD.xml";
-        // String stringUrl =
-        // "http://www.infosyslab.fr/vibrant/project/test/milichia_revision-sdd.xml";
-        // String stringUrl = "http://www.infosyslab.fr/vibrant/project/test/testSDD.xml";
-        // String stringUrl = "http://www.infosyslab.fr/vibrant/project/test/feuillesSDD.xml";
-        // String stringUrl = "http://www.infosyslab.fr/vibrant/project/test/feuillesImagesURL.xml";
-        // String stringUrl = "http://www.infosyslab.fr/vibrant/project/test/smallSDD.xml";
-        // String stringUrl = "http://www.infosyslab.fr/vibrant/project/test/wrongSDD.xml";
-        // String stringUrl = "http://www.infosyslab.fr/vibrant/project/test/pruningSDD.xml";
-        // String stringUrl = "http://www.infosyslab.fr/vibrant/project/test/archaeoSDD.xml";
-        // String stringUrl =
-        // "http://www.infosyslab.fr/vibrant/project/test/varanusSDD_RatingExample.xml";
-        // String stringUrl =
-        // "http://www.infosyslab.fr/vibrant/project/test/varanusSDD_RatingExample3_contextual.xml";
-        // String stringUrl = "http://www.infosyslab.fr/vibrant/project/test/bambousSDD.xml";
-
         // options
-
-        final SDDSaxParser sddSaxParser = new SDDSaxParser(new File("src/test/resources/inputFiles/cichorieae.sdd.xml"), config);
+        final SDDSaxParser sddSaxParser = new SDDSaxParser(new File(testFileUrl), config);
         // construct header
         header.append(System.lineSeparator()).append(sddSaxParser.getDataset().getLabel()).append(", ").append(getBundleConfOverridableElement("message.createdBy")).append(System.lineSeparator());
         header.append(System.lineSeparator()).append("Options:");
-        header.append(System.lineSeparator()).append("sddURL=").append(stringUrl);
+        header.append(System.lineSeparator()).append("sddURL=").append(testFileUrl);
         header.append(System.lineSeparator()).append("fewStatesCharacterFirst=").append(config.isFewStatesCharacterFirst());
         header.append(System.lineSeparator()).append("mergeCharacterStatesIfSameDiscrimination=").append(config.isMergeCharacterStatesIfSameDiscrimination());
         header.append(System.lineSeparator()).append("pruning=").append(config.isPruningEnabled());
@@ -88,16 +76,12 @@ public class IdentificationKeyTXTGeneratorTest {
         header.append(System.lineSeparator()).append("weightContext=").append(config.getWeightContext());
         header.append(System.lineSeparator()).append("weightType=").append(config.getWeightType()).append(System.lineSeparator());
 
-
-        // define parse duration
         final double parseDuration = (double) (System.currentTimeMillis() - beforeTime) / 1000;
-        // define time before processing key
         beforeTime = System.currentTimeMillis();
 
         final IdentificationKeyGenerator identificationKeyGenerator = new IdentificationKeyGenerator(sddSaxParser.getDataset(), config);
         identificationKeyGenerator.createIdentificationKey();
 
-        // define creating key duration
         final double keyCreationDuration = (double) (System.currentTimeMillis() - beforeTime) / 1000;
 
         // construct header
